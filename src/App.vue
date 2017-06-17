1089b25f26b81e1d75954f6c3dd0338d2d6c7acb
han <dongliang.han@12301.cn>
2017-06-17 18:22:54 +0800

    shopping cart

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+     <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app .router-link{
+    width: 1280px;
+    margin: 0 auto;
+    padding:0 30px;
+    text-align: center;
+    background: #605F5F;
+    border-radius: 2px;
+    box-shadow: 5px 2px 5px #eee;
+  }
+  #app .router-link a{
+    display:inline-block;
+    width:150px;
+    height:50px;
+    line-height:50px;
+    font-size:20px;
+    text-align: center;
+    color:#fff;
+  }
+  #app .router-link a:hover{
+    color: #EE7A23;
+  }
+</style>
